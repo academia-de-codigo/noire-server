f50eb763518ceec16a23bb95505cb5812624baf7
Rui Ferrão <mail@ruiferrao.com>
2016-09-12 01:06:55 +0100

    linting, version endpoint testing, travis ci

---
+++ test/version.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Code = require('code'); // the assertions library
+var Lab = require('lab'); // the test framework
+var Server = require('../lib/server');
+var Package = require('../package.json');
+
+var lab = exports.lab = Lab.script(); // export the test script
+
+// make lab feel like jasmine
+var describe = lab.experiment;
+var it = lab.test;
+var expect = Code.expect;
+
+describe('version plugin', function() {
+    it('returns the version from package.json', function(done) {
+        Server.init(8000, function(err, server) {
+
+            expect(err).to.not.exist();
+
+            server.inject('/version', function(response) {
+
+                expect(response.statusCode).to.equal(200);
+                expect(response.result).to.equal({
+                    version: Package.version
+                });
+
+                server.stop(done); // done() callback is required to end the test.
+
+            });
+
+        });
+    });
+});
