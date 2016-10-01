f2400c1a86006355f69a726deb6fbfdffe689815
Rui Ferrão <mail@ruiferrao.com>
2016-10-01 11:53:04 +0100

    server listeners tests

--- test/server.js
+++ test/server.js
@@ -3,8 +3,11 @@
 var Code = require('code'); // the assertions library
 var Lab = require('lab'); // the test framework
 var Hapi = require('hapi');
+var Http = require('http');
+var Https = require('https');
 var Server = require('../lib/server');
 var Version = require('../lib/plugins/version');
+var Config = require('../lib/config');
 var Path = require('path');
 
 var lab = exports.lab = Lab.script(); // export the test script
@@ -15,16 +18,45 @@ var it = lab.test;
 var expect = Code.expect;
 
 var internals = {};
+internals.manifest = {
+    connections: [{
+        host: 'localhost',
+        port: 0,
+        labels: ['web']
+    }, {
+        host: 'localhost',
+        port: 0,
+        labels: ['admin'],
+        tls: Config.tls
+    }, {
+        host: 'localhost',
+        port: 0,
+        labels: ['api'],
+        tls: Config.tls
+    }],
+    registrations: [{
+        plugin: './plugins/version'
+    }]
+};
+
+internals.composeOptions = {
+    relativeTo: Path.resolve(__dirname, '../lib')
+};
 
 describe('Server bootstrap', function() {
 
     it('start server and return server object', function(done) {
 
-        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+        var manifest = {
+            connections: [{
+                port: 0
+            }]
+        };
+
+        Server.init(manifest, internals.composeOptions, function(err, server) {
 
             expect(err).to.not.exist();
             expect(server).to.be.instanceof(Hapi.Server);
-
             server.stop(done);
         });
 
@@ -45,6 +77,58 @@ describe('Server bootstrap', function() {
 
             server.stop(done);
         });
+    });
+
+    it('start server with multiple listeners', function(done) {
+
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+            expect(server).to.be.instanceof(Hapi.Server);
+            expect(server.connections.length).to.equal(3);
+            expect(server.select('web').connections.length).to.equal(1);
+            expect(server.select('admin').connections.length).to.equal(1);
+            expect(server.select('api').connections.length).to.equal(1);
+
+            server.stop(done);
+        });
+
+    });
+
+    it('should use http for listener web', function(done) {
+
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+            expect(server).to.be.instanceof(Hapi.Server);
+            expect(server.select('web').listener instanceof Http.Server).to.equal(true);
+            server.stop(done);
+        });
+
+    });
+
+    it('should use https for listener admin', function(done) {
+
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+            expect(server).to.be.instanceof(Hapi.Server);
+            expect(server.select('admin').listener instanceof Https.Server).to.equal(true);
+            server.stop(done);
+        });
+
+    });
+
+    it('should use https for listener api', function(done) {
+
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+            expect(server).to.be.instanceof(Hapi.Server);
+            expect(server.select('api').listener instanceof Https.Server).to.equal(true);
+            server.stop(done);
+        });
+
     });
 
     it('handles register plugin errors', {
@@ -86,16 +170,3 @@ describe('Server bootstrap', function() {
     });
 
 });
-
-internals.manifest = {
-    connections: [{
-        port: 0
-    }],
-    registrations: [{
-        plugin: './plugins/version'
-    }]
-};
-
-internals.composeOptions = {
-    relativeTo: Path.resolve(__dirname, '../lib')
-};
