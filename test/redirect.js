6f6e65ced76a2b07d9d5787a9cc93cc14247727f
Rui Ferrão <mail@ruiferrao.com>
2016-10-01 10:41:52 +0100

    redirect plugin tests

---
+++ test/redirect.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var Code = require('code'); // the assertions library
+var Lab = require('lab'); // the test framework
+var Path = require('path');
+var Url = require('url');
+var Server = require('../lib/server');
+var Config = require('../lib/config');
+
+var lab = exports.lab = Lab.script(); // export the test script
+
+// make lab feel like jasmine
+var describe = lab.experiment;
+var it = lab.test;
+var expect = Code.expect;
+
+var internals = {
+    adminUrl: {
+        protocol: 'https',
+        slashes: true,
+        hostname: Config.servers.admin.host,
+        port: Config.servers.admin.port
+    },
+    apiUrl: {
+        protocol: 'https',
+        slashes: true,
+        hostname: Config.servers.api.host,
+        port: Config.servers.api.port
+    }
+};
+internals.manifest = {
+    connections: [{
+        port: 0
+    }],
+    registrations: [{
+        plugin: './plugins/admin',
+    }, {
+        plugin: './plugins/redirect'
+    }]
+};
+
+internals.composeOptions = {
+    relativeTo: Path.resolve(__dirname, '../lib')
+};
+
+
+describe('Plugin: redirect', function() {
+
+    it('redirects http api requests to https', function(done) {
+
+        var redirectUrl = Url.format(internals.apiUrl) + Path.resolve(Config.servers.api.prefix, 'version');
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+            server.inject('/api/version', function(response) {
+
+                expect(response.statusCode).to.equal(301);
+                expect(response.statusMessage).to.equal('Moved Permanently');
+                expect(response.headers.location).to.equal(redirectUrl);
+                server.stop(done); // done() callback is required to end the test.
+
+            });
+
+        });
+
+    });
+
+    it('redirects http admin requests to https', function(done) {
+
+        var redirectUrl = Url.format(internals.adminUrl) + Path.resolve(Config.servers.admin.prefix);
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+            server.inject('/admin', function(response) {
+
+                expect(response.statusCode).to.equal(301);
+                expect(response.statusMessage).to.equal('Moved Permanently');
+                expect(response.headers.location).to.equal(redirectUrl);
+                server.stop(done); // done() callback is required to end the test.
+
+            });
+
+        });
+
+    });
+
+});
