372acd48e243aa183c47b4d3d84519d51edf59e1
Rui Ferrão <mail@ruiferrao.com>
2016-10-03 00:22:21 +0100

    login route test

--- test/views.js
+++ test/views.js
@@ -2,9 +2,10 @@
 
 var Code = require('code'); // the assertions library
 var Lab = require('lab'); // the test framework
+var Path = require('path');
+var Config = require('../lib/config');
 var Server = require('../lib/server');
 var Views = require('../lib/plugins/views');
-var Path = require('path');
 
 var lab = exports.lab = Lab.script(); // export the test script
 
@@ -17,7 +18,12 @@ var internals = {};
 
 internals.manifest = {
     connections: [{
-        port: 0
+        port: 0,
+        labels: ['web']
+    }, {
+        port: 0,
+        labels: ['web-tls'],
+        tls: Config.tls
     }],
     registrations: [{
         plugin: './plugins/views'
@@ -57,7 +63,25 @@ describe('Plugin: views', function() {
 
             expect(err).to.not.exist();
 
-            server.inject('/home', function(response) {
+            var web = server.select('web');
+            web.inject('/home', function(response) {
+
+                expect(response.statusCode).to.equal(200);
+                expect(response.result).to.be.a.string();
+            });
+
+            server.stop(done);
+        });
+    });
+
+    it('returns the login view', function(done) {
+
+        Server.init(internals.manifest, internals.composeOptions, function(err, server) {
+
+            expect(err).to.not.exist();
+
+            var webTls = server.select('web-tls');
+            webTls.inject('/login', function(response) {
 
                 expect(response.statusCode).to.equal(200);
                 expect(response.result).to.be.a.string();
