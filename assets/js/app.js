4a4e34b55bb3cf3fe0f2aa48b2fcb7d93c61cba6
Rui Ferrão <mail@ruiferrao.com>
2017-03-05 22:11:16 +0000

    home page using semantic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation();
+$(document).ready(function() {
+    setupNav();
+});
+
+function setupNav() {
+    $('a.item').click(function() {
+        $('.item').removeClass('active');
+        $(this).addClass('active');
+    });
+}
