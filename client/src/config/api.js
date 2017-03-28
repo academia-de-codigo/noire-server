169d63a7d3bb6781d4ad92fdd20786be4ded0a73
Rui Ferrão <mail@ruiferrao.com>
2017-03-28 23:44:24 +0100

    fixed client source linting, user list and row list click to view

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,9 @@
 exports.routes = {
     'login': '/login',
     'logout': '/logout',
-    'update profile': '/profile'
+    'update profile': '/profile',
+    'view user': '/admin/user/{id}',
+    'view role': '/admin/role/{id}',
 };
 
 exports.XHR_OPTIONS = {
--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -1,3 +1,45 @@
 require('../../assets/css/admin.css');
 require('../app');
 require('../commons/nav');
+
+var userTable, roleTable;
+
+$(document).ready(function() {
+
+    grabDomElements();
+    setupUserTableBehaviour();
+    setupRoleTableBehaviour();
+
+});
+
+function grabDomElements() {
+    userTable = $('.ui.user.table');
+    roleTable = $('.ui.role.table');
+}
+
+function setupUserTableBehaviour() {
+
+    // delegate table click events on the corresponding row
+    userTable.on('click', 'tr', function(event) {
+
+        // api can trigger XHR call..
+        /*$(event.currentTarget).api({
+            action: 'get user',
+            on: 'now'
+        });*/
+
+        // .. but actually we want the browser to issue request for a new page
+        var userId = $(event.currentTarget) .attr('data-id');
+        window.location.href = $.fn.api.settings.api['view user'].replace(/{id}/, userId);
+    });
+}
+
+function setupRoleTableBehaviour() {
+
+    // delegate table click events on the corresponding row
+    roleTable.on('click', 'tr', function(event) {
+
+        var roleId = $(event.currentTarget) .attr('data-id');
+        window.location.href = $.fn.api.settings.api['view role'].replace(/{id}/, roleId);
+    });
+}
