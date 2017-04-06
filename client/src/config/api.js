52fed3b33d24edd28449689c2fd4bf71d860ee36
Sérgio Gouveia <sergiogouveia@protonmail.ch>
2017-04-06 15:37:05 +0100

    sort and limit added to the controller

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -4,6 +4,7 @@ exports.routes = {
     'update profile': '/profile',
     'view user': '/admin/user/{id}',
     'view role': '/admin/role/{id}',
+    'list with param': '?sort={/sort}&limit={/limit}'
 };
 
 exports.XHR_OPTIONS = {
--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -2,19 +2,25 @@ require('../../assets/css/admin.css');
 require('../app');
 require('../commons/nav');
 
-var userTable, roleTable;
+var userTable, roleTable, dropdown;
 
 $(document).ready(function() {
 
     grabDomElements();
     setupUserTableBehaviour();
     setupRoleTableBehaviour();
+    setupDropdown();
 
 });
 
 function grabDomElements() {
     userTable = $('.ui.user.table');
     roleTable = $('.ui.role.table');
+    dropdown = $('.ui.dropdown');
+}
+
+function setupDropdown() {
+    dropdown.dropdown();
 }
 
 function setupUserTableBehaviour() {
@@ -29,7 +35,7 @@ function setupUserTableBehaviour() {
         });*/
 
         // .. but actually we want the browser to issue request for a new page
-        var userId = $(event.currentTarget) .attr('data-id');
+        var userId = $(event.currentTarget).attr('data-id');
         window.location.href = $.fn.api.settings.api['view user'].replace(/{id}/, userId);
     });
 }
@@ -39,7 +45,7 @@ function setupRoleTableBehaviour() {
     // delegate table click events on the corresponding row
     roleTable.on('click', 'tbody tr', function(event) {
 
-        var roleId = $(event.currentTarget) .attr('data-id');
+        var roleId = $(event.currentTarget).attr('data-id');
         window.location.href = $.fn.api.settings.api['view role'].replace(/{id}/, roleId);
     });
 }
