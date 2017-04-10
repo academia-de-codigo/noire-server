93286b2b1abe8a83e27a6cc2f09c515226d8c9e9
Pedro Antoninho <pdn.antoninho@gmail.com>
2017-04-10 15:13:58 +0100

    fix tests and merge conflicts

--- webpack/dev/plugins/copy-files.js
+++ webpack/dev/plugins/copy-files.js
@@ -6,14 +6,22 @@ module.exports = function(options) {
     var paths = options.paths;
 
     var config = [{
+        // copy all img assets from client/assets/img to dist/img
         context: paths.src.path,
         from: Path.join(paths.src.assets, paths.assets.img),
         to: paths.output.img
     }, {
+        // copy all font assets from client/assets/fonts to dist/fonts
         context: paths.src.path,
         from: Path.join(paths.src.assets, paths.assets.fonts),
         to: paths.output.fonts
     }, {
+        // copy all files in the client/assets folder to dist/ folder
+        context: Path.join(paths.src.path, paths.src.assets),
+        from: '*.*',
+        to: Path.join(paths.output.path)
+    }, {
+        // copy all partials from client/views/partials to dist/views/partials
         context: paths.src.path,
         from: Path.join(paths.src.views, paths.views.partials),
         to: Path.join(paths.output.views, paths.views.partials)
