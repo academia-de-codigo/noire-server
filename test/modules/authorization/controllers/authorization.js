e6ab2507ec71b2fcb7dda03d979ee5fbf43c5f0a
Rui Ferrão <mail@ruiferrao.com>
2017-12-21 17:55:06 +0000

    authorization controller and tests updated to es6 and modern hapi style

---
+++ test/modules/authorization/controllers/authorization.js
@@ -0,0 +1,113 @@
+const Path = require('path');
+const Lab = require('lab');
+var Sinon = require('sinon');
+const Hapi = require('hapi');
+const AuthorizationController = require(Path.join(process.cwd(), 'lib/modules/authorization/controllers/authorization'));
+const AuthorizationService = require(Path.join(process.cwd(), 'lib/modules/authorization/services/authorization'));
+
+const { afterEach, beforeEach, describe, expect, it } = exports.lab = Lab.script();
+
+describe('Controller: Authorization', () => {
+
+    const action = 'read';
+    const resource = 'role';
+    const username = 'username';
+    const fakeResult = 'ok';
+    const fakeRoute = { path: '/', method: 'GET', config: { handler: () => fakeResult } };
+
+    let server;
+    let authorizationStub;
+
+    beforeEach(() => {
+
+        server = Hapi.server();
+        authorizationStub = Sinon.stub(AuthorizationService, 'canUser');
+    });
+
+    afterEach(() => {
+
+        authorizationStub.restore();
+    });
+
+    it('authorizes user to access resource with explicit action', async () => {
+
+        // setup
+        authorizationStub.withArgs(username, action, resource).returns(Promise.resolve(true));
+
+        // exercise
+        fakeRoute.config.pre = [AuthorizationController.authorize(resource, action)];
+        server.route(fakeRoute);
+        const response = await server.inject({
+            url: '/',
+            credentials: {
+                username
+            }
+        });
+
+        // validate
+        expect(response.statusCode).to.equal(200);
+        expect(response.result).to.equals(fakeResult);
+    });
+
+    it('authorizes user to access resource with implicit action', async () => {
+
+        // setup
+        authorizationStub.withArgs(username, action, resource).returns(Promise.resolve(true));
+
+        // exercise
+        fakeRoute.config.pre = [AuthorizationController.authorize(resource)];
+        server.route(fakeRoute);
+        const response = await server.inject({
+            url: '/',
+            credentials: {
+                username
+            }
+        });
+
+        // validate
+        expect(response.statusCode).to.equal(200);
+        expect(response.result).to.equals(fakeResult);
+    });
+
+    it('does not authorize user to access resource', async () => {
+
+        // setup
+        authorizationStub.withArgs(username, action, resource).returns(Promise.resolve(false));
+
+        // exercise
+        fakeRoute.config.pre = [AuthorizationController.authorize(resource)];
+        server.route(fakeRoute);
+        const response = await server.inject({
+            url: '/',
+            credentials: {
+                username
+            }
+        });
+
+        // validate
+        expect(response.statusCode).to.equals(403);
+        expect(response.statusMessage).to.equals('Forbidden');
+        expect(JSON.parse(response.payload).message).to.equals('insufficient privileges');
+    });
+
+    it('handles errors while authorizing', async () => {
+
+        // setup
+        authorizationStub.withArgs(username, action, resource).returns(Promise.reject(new Error()));
+
+        // exercise
+        fakeRoute.config.pre = [AuthorizationController.authorize(resource)];
+        server.route(fakeRoute);
+        const response = await server.inject({
+            url: '/',
+            credentials: {
+                username
+            }
+        });
+
+        // validate
+        expect(response.statusCode).to.equals(500);
+        expect(response.statusMessage).to.equals('Internal Server Error');
+        expect(JSON.parse(response.payload).message).to.equals('An internal server error occurred');
+    });
+});
