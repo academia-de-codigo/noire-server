8f7fff34ef936f335370b10d44012df39bd48274
Audrey <audrey.lopes@academiadecodigo.org>
2019-02-18 18:00:37 +0000

    added tests for env dependent config

--- .labrc.js
+++ .labrc.js
@@ -7,7 +7,7 @@ module.exports = {
     assert: 'code',
     'coverage-path': 'lib',
     'coverage-exclude': ['config.js', 'config-views.js'],
-    paths: ['test/errors', 'test/utils', 'test/models', 'test/plugins', 'test/modules'],
+    paths: ['test/config','test/errors', 'test/utils', 'test/models', 'test/plugins', 'test/modules'],
     // TODO: track/fix global leaks
     // __core-js_shared__ -> https://github.com/tgriesser/knex/issues/1720
     globals: 'core,regeneratorRuntime,_babelPolyfill,__core-js_shared__'
